b6ad0f746208fa528c199045ff4e9ceac1614465
Krystian <krystian.k.nowak@gmail.com>
2019-05-02 20:09:53 +0200

    init

---
+++ Example1.js
@@ -0,0 +1,53 @@
+class Example1 extends Phaser.Scene {
+    constructor() {
+        super({key: 'Example1'});
+    }
+
+    preload() {
+        this.load.image('pad', 'assets/control.png')
+        this.load.image('pad2', 'assets/pad.jpg')
+    }
+
+    create() {
+        this.image = this.add.image(400, 300, 'pad');
+        // this.image.scaleTo(0.5,0.5);
+        this.image.displayWidth = 250;
+        this.image.displayHeight = 250;
+        // this.image.height = 250;
+
+        this.input.keyboard.on('keyup_D', (event) => {
+            this.image.x += 10;
+        })
+
+        this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+
+        // this.input.keyboard.on('keyup_A', (event) => {
+        //     this.image.x -= 10;
+        // })
+
+        this.input.on('pointerdown', (event) => {
+            this.image.x = event.x;
+            this.image.y = event.y;
+        });
+
+        this.input.keyboard.on('keyup_P', (event) => {
+            let physicsImage = this.physics.add.image(this.image.x, this.image.y, 'pad');
+            // this.physicsImage.displayWidth = 250;
+            // this.physicsImage.displayHeight = 250;
+            physicsImage.setVelocity(Phaser.Math.RND.integerInRange(-100, 100), -300);
+            // physicsImage.setVelocity(300, -200);
+        });
+
+        this.input.keyboard.on('keyup', (event) => {
+            if(event.key == "2") {
+                this.scene.start("Example2");
+            }
+        });
+    }
+
+    update(delta) {
+        if(this.key_A.isDown)
+            this.image.x--;
+        // if(this.input.keyboard.)
+    }
+}
---
+++ Example2.js
@@ -0,0 +1,26 @@
+// import {Phaser} from 'phaser';
+
+class Example2 extends Phaser.Scene {
+    constructor() {
+        super({key: "Example2"});
+    }
+
+    create() {
+        this.text = this.add.text(0,0, "Welcome to Example2!!", {font: "40px Impact"});
+
+        var tween = this.tweens.add({
+            targets: this.text,
+            x:200,
+            y:200,
+            duration: 2000,
+            ease: "Elastic",
+            easeParams: [1,5, 0.5],
+            delay: 1000,
+            onComplete: (src, trgt) => {
+                trgt[0].x = 0;
+                trgt[0].y = 0;
+                trgt[0].setColor("Cyan");
+            }
+        });
+    }
+}
